7e9e9e488e64103be8dfb706b89d1c7ea9f2e308
Arnout <schepens.arnout@gmail.com>
2017-05-25 13:50:45 +0200

    Added Template Pattern

---
+++ Template Pattern/src/ducks/main/StartUp.java
@@ -0,0 +1,39 @@
+package ducks.main;
+
+import java.util.Arrays;
+
+import ducks.domain.Duck;
+
+public class StartUp
+{
+
+	public static void main(String[] args)
+	{
+		Duck[] ducks =
+		{
+			new Duck("Daffy", 8),
+			new Duck("Dewey", 2),
+			new Duck("Howard", 7),
+			new Duck("Louie", 2),
+			new Duck("Donald", 10),
+			new Duck("Huey", 2)
+		};
+		
+		System.out.println("Before sorting:");
+		display(ducks);
+		
+		Arrays.sort(ducks);
+		
+		System.out.println("\nAfter sorting");
+		display(ducks);
+	}
+
+	private static void display(Duck[] ducks)
+	{
+		for(int i = 0; i < ducks.length; i++)
+		{
+			System.out.println(ducks[i]);
+		}
+	}
+
+}
